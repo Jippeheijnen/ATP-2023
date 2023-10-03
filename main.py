f6a240caea42aa7b3dac2dcdf1afcf9405264962
Jippe Heijnen <jippeheijnen@gmail.com>
2023-10-03 23:48:59 +0200

    add some classes and dummy functions

--- main.py
+++ main.py
@@ -1,72 +1,67 @@
-import unittest
+import time
+
+from stepperdriver import TMC2209
+from communication import Serial
+from util import Pins
+from uvsensor import VEML6075
 
 
-def log_method_calls(func):
+# Decorator for logging
+def log_data(func):
     def wrapper(*args, **kwargs):
-        print(f"Calling {func.__name__} with args: {args} and kwargs: {kwargs}")
+        start_time = time.time()
         result = func(*args, **kwargs)
-        print(f"{func.__name__} returned: {result}")
+        end_time = time.time()
+        print(f"{func.__name__} executed in {end_time - start_time} seconds")
         return result
 
     return wrapper
 
 
-# Decorator for mocking hardware interactions during testing
-def mock_hardware(func):
-    def wrapper(*args, **kwargs):
-        # Implement mock behavior here
-        # For example, return predefined values or raise exceptions as needed
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
-class MockupUVSensor:
-    def __init__(self):
-        self.connected = False
-
-    @log_method_calls
-    @mock_hardware
-    def connect(self):
-        # Simulate hardware connection logic
-        self.connected = True
-
-    @log_method_calls
-    @mock_hardware
-    def disconnect(self):
-        # Simulate hardware disconnection logic
-        self.connected = False
-
-    @log_method_calls
-    @mock_hardware
-    def send_data(self, data):
-        if not self.connected:
-            raise RuntimeError("Not connected to hardware")
-        # Simulate sending data to mockup hardware
-        return f"Sent: {data}"
-
-
-class TestMockupHardware(unittest.TestCase):
-    @mock_hardware
-    def test_connect(self):
-        hardware = MockupUVSensor()
-        hardware.connect()
-        self.assertTrue(hardware.connected)
-
-    @mock_hardware
-    def test_disconnect(self):
-        hardware = MockupUVSensor()
-        hardware.connect()
-        hardware.disconnect()
-        self.assertFalse(hardware.connected)
-
-    @mock_hardware
-    def test_send_data(self):
-        hardware = MockupUVSensor()
-        hardware.connect()
-        result = hardware.send_data("Hello, hardware")
-        self.assertEqual(result, "Sent: Hello, hardware")
-
-
 if __name__ == '__main__':
-    unittest.main()
+    # Constants for calibration and tracking
+    TARGET_UV_THRESHOLD = 1000  # Adjust this threshold based on your calibration
+    HORIZONTAL_SPEED = 0.5  # Adjust the speed as needed
+    VERTICAL_SPEED = 0.3  # Adjust the speed as needed
+
+    tx_steppers = Pins.A1
+    rx_steppers = Pins.A2
+
+    soft_serial = Serial(tx_steppers, rx_steppers)
+
+    # Initialize UV sensors
+    uv_sensors = [VEML6075(1, soft_serial),
+                  VEML6075(2, soft_serial),
+                  VEML6075(3, soft_serial),
+                  VEML6075(4, soft_serial)]
+
+    # Initialize motors
+    horizontal_motor = TMC2209(1, soft_serial)
+    vertical_motor = TMC2209(2, soft_serial)
+
+
+    # Function to track the sun
+    @log_data
+    def track_sun():
+        while True:
+            # hieronder wat voorbeeld logica
+            uv_values = [sensor.read_uv() for sensor in uv_sensors]
+            # avg_uv_value = sum(uv_values) / len(uv_values)
+
+            # Check if the average UV value is above the threshold
+            # if avg_uv_value > TARGET_UV_THRESHOLD:
+            if True:
+                # Calculate adjustments for horizontal and vertical motors
+                horizontal_adjustment = 0  # Calculate based on sensor data
+                vertical_adjustment = 0  # Calculate based on sensor data
+
+                # Move the motors to adjust the solar panel position
+                horizontal_motor.move(HORIZONTAL_SPEED * horizontal_adjustment)
+                vertical_motor.move(VERTICAL_SPEED * vertical_adjustment)
+                # Move the motors to adjust the solar panel position
+                horizontal_motor.move(HORIZONTAL_SPEED * horizontal_adjustment)
+                vertical_motor.move(VERTICAL_SPEED * vertical_adjustment)
+            time.sleep(1)  # Adjust the sleep duration as needed
+
+    if __name__ == "__main__":
+        track_sun()
